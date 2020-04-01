3b49111246b55257ee220cb964e049f540e7886a
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2020-03-31 18:28:12 -0700

    started

---
+++ data-structures/linkedlist/linked-list.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* 
+
+Your Node class should have the following properties:
+
+val - The value stored in the Node
+next - A pointer the next Node in the list
+prev - A pointer to the previous Node in the list (for use in stretch goal implementation)
+
+Your LinkedList class should have the following properties:
+
+head - The Node that represents the beginning or start of the list
+Your LinkedList class should have the following functions/methods:
+
+insert() - A function that takes any value as an argument and adds a new Node with that value to the beginning of the list (before the current head
+
+includes() - A function that takes in a value as an argument and returns true or false if that value was found in the list
+toString() - A function that takes no arguments but prints out a visual representation of the Linked List, similar to the format: { a } -> { b } -> { c } -> NULL
+Note that any errors in your LinkedList class and methods should be well handled and logged. Don’t default to the standard thrown error; instead use try catch blocks to correctly catch errors and log a user-friendly and descriptive message to the console.
+
+*/ 
